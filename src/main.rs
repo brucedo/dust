bb72796effad76b230763bdf9f546085526834c5
Doug Bruce <queazan@gmail.com>
2024-04-06 16:13:20 -0500

    - Misunderstood how extensions were queried.  Am now seeing the core vulkan extensions.  hoo ray.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use ash::{
-    vk::{ApplicationInfo, ApplicationInfoBuilder, InstanceCreateInfo, InstanceCreateInfoBuilder},
+    vk::{ApplicationInfo, InstanceCreateInfo},
     Entry,
 };
 use log::debug;
@@ -12,20 +12,22 @@ fn main() {
 
     scan(&vk_entry);
 
+    let app_name = &CString::new("Dust").unwrap();
+
     let app_info_bldr = ApplicationInfo::default()
-        .application_name(&CString::new("Dust").unwrap())
+        .application_name(app_name)
         .application_version(1)
         .api_version(ash::vk::make_api_version(0, 1, 3, 0))
-        .engine_name(&CString::new("Dust").unwrap())
+        .engine_name(app_name)
         .engine_version(1);
 
     let instance_info_bldr = InstanceCreateInfo::default().application_info(&app_info_bldr);
 
-    let instance = unsafe { vk_entry.create_instance(&instance_info_bldr, allocation_callbacks) };
+    // let instance = unsafe { vk_entry.create_instance(&instance_info_bldr, allocation_callbacks) };
 }
 
 fn scan(vk_entry: &Entry) {
-    match vk_entry.try_enumerate_instance_version() {
+    match unsafe { vk_entry.try_enumerate_instance_version() } {
         Ok(version_opt) => {
             if let Some(version) = version_opt {
                 println!(
@@ -43,44 +45,49 @@ fn scan(vk_entry: &Entry) {
         }
     }
 
-    let vk_layer_props = match vk_entry.enumerate_instance_layer_properties() {
+    let vk_layer_props = match unsafe { vk_entry.enumerate_instance_layer_properties() } {
         Ok(props) => props,
         Err(msg) => {
             panic!("ERR: {}", msg);
         }
     };
 
-    let props: Vec<&CStr> = vk_layer_props
+    let mut layer_names: Vec<Option<&CStr>> = vk_layer_props
         .iter()
         .map(|prop| unsafe { CStr::from_ptr(prop.layer_name.as_ptr()) })
+        .map(|prop_name| Some(prop_name))
         .collect();
 
-    debug!("Size of found props: {}", props.len());
+    layer_names.push(None);
 
-    for prop in props {
-        debug!("{:?}", prop);
-        match vk_entry.enumerate_instance_extension_properties(Some(prop)) {
+    debug!("Size of found props: {}", layer_names.len());
+
+    for layer_name in layer_names {
+        debug!("{:?}", layer_name);
+        match unsafe { vk_entry.enumerate_instance_extension_properties(layer_name) } {
             Ok(ext_props) => {
                 debug!(
                     "Inspecting layer {:?} properties.  Count: {}",
-                    prop,
+                    layer_name,
                     ext_props.len()
                 );
                 for ext_prop in ext_props {
                     let prop_name_cstr =
                         unsafe { CStr::from_ptr(ext_prop.extension_name.as_ptr()) };
-                    if let (Ok(prop_str), Ok(prop_ext_str)) =
-                        (prop.to_str(), prop_name_cstr.to_str())
-                    {
+                    let layer_name_str = match layer_name {
+                        Some(cstr) => cstr.to_str().unwrap(),
+                        None => "NONE",
+                    };
+                    if let Ok(prop_ext_str) = prop_name_cstr.to_str() {
                         debug!(
                             "Found extension property {} for layer {}",
-                            prop_ext_str, prop_str
+                            prop_ext_str, layer_name_str
                         );
                     }
                 }
             }
             Err(msg) => {
-                panic!("ERR for {}: {}", prop.to_str().unwrap(), msg)
+                panic!("ERR for {:?}: {}", layer_name.unwrap(), msg)
             }
         }
     }
