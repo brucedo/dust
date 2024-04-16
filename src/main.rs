509e3fb86938a9b7764a124b554b4be06a010e5e
Doug Bruce <queazan@gmail.com>
2024-04-15 19:42:29 -0500

    - Started moving vulkan setup to setup submodule

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,17 @@
-mod setup;
-
-use ash::{
-    vk::{ApplicationInfo, InstanceCreateInfo},
-    Entry,
-};
 use log::debug;
-use std::ffi::{CStr, CString};
+
+mod setup;
 
 fn main() {
     env_logger::init();
 
-    let vk_entry = unsafe { ash::Entry::load() }.unwrap();
+    let entry = setup::instance::init();
+    let instance = setup::instance::instance(&entry);
 
-    scan(&vk_entry);
-
-    let app_name = &CString::new("Dust").unwrap();
-
-    let app_info_bldr = ApplicationInfo::default()
-        .application_name(app_name)
-        .application_version(1)
-        .api_version(ash::vk::make_api_version(0, 1, 3, 0))
-        .engine_name(app_name)
-        .engine_version(1);
-
-    let instance = setup::instance::instance(&vk_entry);
+    debug!("The...instance was created?");
 
     // let instance = unsafe { vk_entry.create_instance(&instance_info_bldr, allocation_callbacks) };
     unsafe { instance.destroy_instance(None) };
-}
 
-fn scan(vk_entry: &Entry) {
-    match unsafe { vk_entry.try_enumerate_instance_version() } {
-        Ok(version_opt) => {
-            if let Some(version) = version_opt {
-                println!(
-                    "Vulkan version detected: {}.{}.{}",
-                    ash::vk::api_version_major(version),
-                    ash::vk::api_version_minor(version),
-                    ash::vk::api_version_patch(version),
-                )
-            } else {
-                println!("None version returned")
-            }
-        }
-        Err(msg) => {
-            println!("VkResult::err: {}", msg)
-        }
-    }
-
-    let vk_layer_props = match unsafe { vk_entry.enumerate_instance_layer_properties() } {
-        Ok(props) => props,
-        Err(msg) => {
-            panic!("ERR: {}", msg);
-        }
-    };
-
-    let mut layer_names: Vec<Option<&CStr>> = vk_layer_props
-        .iter()
-        .map(|prop| unsafe { CStr::from_ptr(prop.layer_name.as_ptr()) })
-        .map(|prop_name| Some(prop_name))
-        .collect();
-
-    layer_names.push(None);
-
-    debug!("Size of found props: {}", layer_names.len());
-
-    for layer_name in layer_names {
-        debug!("{:?}", layer_name);
-        match unsafe { vk_entry.enumerate_instance_extension_properties(layer_name) } {
-            Ok(ext_props) => {
-                debug!(
-                    "Inspecting layer {:?} properties.  Count: {}",
-                    layer_name,
-                    ext_props.len()
-                );
-                for ext_prop in ext_props {
-                    let prop_name_cstr =
-                        unsafe { CStr::from_ptr(ext_prop.extension_name.as_ptr()) };
-                    let layer_name_str = match layer_name {
-                        Some(cstr) => cstr.to_str().unwrap(),
-                        None => "NONE",
-                    };
-                    if let Ok(prop_ext_str) = prop_name_cstr.to_str() {
-                        debug!(
-                            "Found extension property {} for layer {}",
-                            prop_ext_str, layer_name_str
-                        );
-                    }
-                }
-            }
-            Err(msg) => {
-                panic!("ERR for {:?}: {}", layer_name.unwrap(), msg)
-            }
-        }
-    }
+    debug!("Vulkan instance destroyed...");
 }
--- src/setup/instance.rs
+++ src/setup/instance.rs
@@ -1,10 +1,22 @@
 use ash::vk::{ApplicationInfo, InstanceCreateInfo};
+use ash::Entry;
+use core::panic;
+use log::debug;
 use std::ffi::{CStr, CString};
+
+pub fn init() -> ash::Entry {
+    debug!("Starting initialization");
+    match unsafe { ash::Entry::load() } {
+        Ok(entry) => entry,
+        Err(msg) => {
+            panic!("The Vulkan initialization process has failed: {}", msg);
+        }
+    }
+}
 
 #[cfg(all(target_os = "linux", not(target_os = "windows")))]
 pub fn instance(entry: &ash::Entry) -> ash::Instance {
-    use log::debug;
-
+    debug!("Starting instance creation...");
     let app_name = CString::new("Dust for Linux").unwrap();
     let khr_surface_name = CString::new("VK_KHR_surface").unwrap();
     let khr_xcb_surface_name = CString::new("VK_KHR_xcb_surface").unwrap();
@@ -13,14 +25,23 @@ pub fn instance(entry: &ash::Entry) -> ash::Instance {
         khr_xcb_surface_name.as_c_str().as_ptr(),
     ];
 
+    debug!("Extension names setup...");
+
     let app_info = app_info(app_name.as_c_str());
+
+    debug!("App info struct filled");
 
     let instance_info = InstanceCreateInfo::default()
         .application_info(&app_info)
         .enabled_extension_names(&xcb_ext_name);
 
+    debug!("instance_info struct filled");
+
     match unsafe { entry.create_instance(&instance_info, None) } {
-        Ok(instance) => instance,
+        Ok(instance) => {
+            debug!("Instance successfully created?");
+            instance
+        }
         Err(msg) => panic!("Instance creation failed: {}", msg),
     }
 }
@@ -36,11 +57,78 @@ pub fn instance() {
     panic!("No support for OSes other than Windows or Linux")
 }
 
-pub fn app_info(app_name: &CStr) -> ApplicationInfo {
+fn app_info(app_name: &CStr) -> ApplicationInfo {
     ApplicationInfo::default()
         .application_name(app_name)
         .application_version(1)
         .api_version(ash::vk::make_api_version(0, 1, 3, 0))
         .engine_name(app_name)
         .engine_version(1)
+}
+
+fn scan(vk_entry: &Entry) {
+    match unsafe { vk_entry.try_enumerate_instance_version() } {
+        Ok(version_opt) => {
+            if let Some(version) = version_opt {
+                println!(
+                    "Vulkan version detected: {}.{}.{}",
+                    ash::vk::api_version_major(version),
+                    ash::vk::api_version_minor(version),
+                    ash::vk::api_version_patch(version),
+                )
+            } else {
+                println!("None version returned")
+            }
+        }
+        Err(msg) => {
+            println!("VkResult::err: {}", msg)
+        }
+    }
+
+    let vk_layer_props = match unsafe { vk_entry.enumerate_instance_layer_properties() } {
+        Ok(props) => props,
+        Err(msg) => {
+            panic!("ERR: {}", msg);
+        }
+    };
+
+    let mut layer_names: Vec<Option<&CStr>> = vk_layer_props
+        .iter()
+        .map(|prop| unsafe { CStr::from_ptr(prop.layer_name.as_ptr()) })
+        .map(|prop_name| Some(prop_name))
+        .collect();
+
+    layer_names.push(None);
+
+    debug!("Size of found props: {}", layer_names.len());
+
+    for layer_name in layer_names {
+        debug!("{:?}", layer_name);
+        match unsafe { vk_entry.enumerate_instance_extension_properties(layer_name) } {
+            Ok(ext_props) => {
+                debug!(
+                    "Inspecting layer {:?} properties.  Count: {}",
+                    layer_name,
+                    ext_props.len()
+                );
+                for ext_prop in ext_props {
+                    let prop_name_cstr =
+                        unsafe { CStr::from_ptr(ext_prop.extension_name.as_ptr()) };
+                    let layer_name_str = match layer_name {
+                        Some(cstr) => cstr.to_str().unwrap(),
+                        None => "NONE",
+                    };
+                    if let Ok(prop_ext_str) = prop_name_cstr.to_str() {
+                        debug!(
+                            "Found extension property {} for layer {}",
+                            prop_ext_str, layer_name_str
+                        );
+                    }
+                }
+            }
+            Err(msg) => {
+                panic!("ERR for {:?}: {}", layer_name.unwrap(), msg)
+            }
+        }
+    }
 }
