dea06706a619e846f68982ad47e6bcf07ead8116
Doug Bruce <queazan@gmail.com>
2024-05-27 18:16:14 +1000

    - Adding the key mapper trait...

--- src/setup/key_mapper.rs
+++ src/setup/key_mapper.rs
@@ -1,1 +1,4 @@
-pub trait KeyMapper {}
+pub trait KeyMapper {
+    update_state(&mut self, depressed_keys: &[u32], engaged_toggles: [&u32]);
+    translate_keypress(&mut self, key_stroke: u32) -> Option<String>;
+}
