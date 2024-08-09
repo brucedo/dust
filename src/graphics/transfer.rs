bd729713fc2094a82afd5287430db0b7fc0d67a4
Doug Bruce <queazan@gmail.com>
2024-08-09 16:47:15 -0500

    - Filling out transfer a bit now.

--- src/graphics/transfer.rs
+++ src/graphics/transfer.rs
@@ -1,9 +1,10 @@
-use std::mem::size_of;
+use std::{array, mem::size_of, usize};
 
 use crate::dust_errors::DustError;
 use ash::vk::{
-    Buffer, BufferCreateFlags, BufferCreateInfo, BufferUsageFlags, MemoryPropertyFlags,
-    PhysicalDeviceMemoryProperties, SharingMode,
+    Buffer, BufferCreateFlags, BufferCreateInfo, BufferUsageFlags, DeviceMemory,
+    MemoryAllocateInfo, MemoryMapFlags, MemoryPropertyFlags, PhysicalDeviceMemoryProperties,
+    SharingMode,
 };
 use log::debug;
 
@@ -11,35 +12,61 @@ use crate::setup::instance::VkContext;
 
 pub fn copy_to_buffer<T>(data: &[T], ctxt: &VkContext) -> Buffer
 where
-    T: Sized,
+    T: Sized + Copy + Clone,
 {
-    let host_facing_buffer_info = BufferCreateInfo::default()
-        .size(size_of::<T>() as u64)
-        .usage(BufferUsageFlags::TRANSFER_SRC | BufferUsageFlags::TRANSFER_DST)
-        .sharing_mode(SharingMode::EXCLUSIVE)
-        .flags(BufferCreateFlags::empty());
+    let size_in_bytes = std::mem::size_of_val(data) as u64;
 
-    let transfer_buffer = match unsafe {
-        ctxt.logical_device
-            .create_buffer(&host_facing_buffer_info, None)
-    } {
-        Ok(buffer) => buffer,
-        Err(msg) => {
-            panic!("Transfer buffer creation failed: {:?}", msg);
-        }
-    };
+    let transfer_buffer = make_buffer(
+        ctxt,
+        size_in_bytes,
+        BufferUsageFlags::TRANSFER_SRC | BufferUsageFlags::TRANSFER_DST,
+    );
 
-    let reqs = unsafe {
-        ctxt.logical_device
-            .get_buffer_memory_requirements(transfer_buffer)
-    };
-
-    let properties = match match_memory_type(
-        &ctxt.physical_memory_properties,
-        reqs.memory_type_bits,
+    let mem_handle = back_buffer_with_memory(
+        ctxt,
+        &transfer_buffer,
+        size_in_bytes,
         &(MemoryPropertyFlags::HOST_VISIBLE
             | MemoryPropertyFlags::HOST_COHERENT
             | MemoryPropertyFlags::DEVICE_LOCAL),
+    );
+
+    let void_ptr = match unsafe {
+        ctxt.logical_device
+            .map_memory(mem_handle, 0, size_in_bytes, MemoryMapFlags::empty())
+    } {
+        Ok(ptr) => ptr,
+        Err(msg) => {
+            panic!("Failed to map the buffer backed memory to host: {:?}", msg);
+        }
+    };
+
+    let t_ptr = void_ptr as *mut T;
+    // let u8_ptr = void_ptr as *mut u8;
+    let t_buf = unsafe { std::slice::from_raw_parts_mut(t_ptr, size_in_bytes as usize) };
+
+    t_buf.copy_from_slice(data);
+
+    unsafe {
+        ctxt.logical_device.destroy_buffer(transfer_buffer, None);
+    }
+
+    transfer_buffer
+}
+
+fn back_buffer_with_memory(
+    ctxt: &VkContext,
+    buffer: &Buffer,
+    buffer_size: u64,
+    desired_properties: &MemoryPropertyFlags,
+) -> DeviceMemory {
+    let transfer_memory_requirements =
+        unsafe { ctxt.logical_device.get_buffer_memory_requirements(*buffer) };
+
+    let transfer_mem_type_index = match match_memory_type(
+        &ctxt.physical_memory_properties,
+        transfer_memory_requirements.memory_type_bits,
+        desired_properties,
     ) {
         Ok(props) => props,
         Err(msg) => {
@@ -47,9 +74,51 @@ where
         }
     };
 
-    unsafe {
-        ctxt.logical_device.destroy_buffer(transfer_buffer, None);
+    let mem_alloc_info = MemoryAllocateInfo::default()
+        .allocation_size(buffer_size)
+        .memory_type_index(transfer_mem_type_index);
+
+    let mem_handle = match unsafe { ctxt.logical_device.allocate_memory(&mem_alloc_info, None) } {
+        Ok(handle) => handle,
+        Err(msg) => {
+            panic!("Unable to allocate buffer sized {}: {:?}", buffer_size, msg);
+        }
+    };
+
+    match unsafe {
+        ctxt.logical_device
+            .bind_buffer_memory(*buffer, mem_handle, 0)
+    } {
+        Ok(_) => {}
+        Err(msg) => {
+            panic!(
+                "Failed to bind the buffer to the allocated memory: {:?}",
+                msg
+            );
+        }
     }
+
+    mem_handle
+}
+
+fn make_buffer(ctxt: &VkContext, buffer_size: u64, flags: BufferUsageFlags) -> Buffer {
+    let transfer_buffer_info = BufferCreateInfo::default()
+        .size(buffer_size)
+        .usage(flags)
+        .sharing_mode(SharingMode::EXCLUSIVE)
+        .flags(BufferCreateFlags::empty());
+
+    let transfer_buffer = match unsafe {
+        ctxt.logical_device
+            .create_buffer(&transfer_buffer_info, None)
+    } {
+        Ok(buffer) => buffer,
+        Err(msg) => {
+            panic!("Transfer buffer creation failed: {:?}", msg);
+        }
+    };
+
+    transfer_buffer
 }
 
 pub fn match_memory_type(
