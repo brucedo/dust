0b6ce92569e564a4d18984dae3a21774abd0db51
Doug Bruce <queazan@gmail.com>
2024-10-13 23:55:06 -0500

    - Okay.  I think I have the semblance of a plan for properly    handling memory management in vulkan.  I hope anyways.

--- src/dust_errors.rs
+++ src/dust_errors.rs
@@ -3,5 +3,6 @@ use ash::vk::Result;
 #[derive(Debug)]
 pub enum DustError {
     NoMatchingMemoryType,
+    DeviceMemoryAllocationFailed(Result),
     CreateShaderModuleFailed(Result),
 }
--- src/graphics/object_store.rs
+++ src/graphics/object_store.rs
@@ -1,33 +1,115 @@
-use ash::vk::{DeviceMemory, MemoryAllocateInfo, MemoryPropertyFlags};
+use ash::vk::{Buffer, DeviceMemory, Image, MemoryAllocateInfo, MemoryPropertyFlags, MemoryType};
 
-use crate::setup::instance::VkContext;
+use log::debug;
+
+use std::sync::Arc;
+
+use crate::{dust_errors::DustError, setup::instance::VkContext};
 
 use super::{image::DustImage, transfer::match_memory_type};
 
-pub struct DustObjectStore {
-    images: [Option<DustImage>; 1000],
-    free_image_slots: Vec<usize>,
-    free_buffer_slots: Vec<usize>,
-    heap: DeviceMemory,
-    unused_offset: usize,
+pub type ImageId = usize;
+pub type BufferId = usize;
+
+struct DeviceMemoryTombstone {
+    pub device_meory: DeviceMemory,
+    pub size: usize,
+    pub first_free: usize,
 }
 
-pub fn new(ctxt: &VkContext) -> DustObjectStore {
+struct ImageTombstone {
+    image: Image,
+    heap_type_index: usize,
+    heap_index: usize,
+    allocated_offset: usize,
+    allocation_size: usize,
+}
 
+struct BufferTombstone {
+    buffer: Buffer,
+    heap_type_index: usize,
+    heap_index: usize,
+    allocated_offset: usize,
+    alloation_size: usize,
+}
 
-    let memory_type_index = ctxt.match_memory_type(, MemoryPropertyFlags::DEVICE_LOCAL) ;
+pub struct DustObjectStore {
+    ctxt: Arc<VkContext>,
+    heap_growth: Vec<usize>,
+    heaps: Vec<Vec<DeviceMemoryTombstone>>,
+    types: Vec<MemoryType>,
+}
 
-    let allocate_memory_info = MemoryAllocateInfo::default()
-        .allocation_size(104857600)
-        .memory_type_index(memory_type_index)
-    ctxt.logical_device.allocate_memory(allocate_info, allocation_callbacks)
+pub fn new(ctxt: Arc<VkContext>) -> DustObjectStore {
+    let mut type_vec = Vec::new();
+    let mut heaps = Vec::new();
+    let mut heap_growth = Vec::new();
+    for memory_type in ctxt.physical_memory_properties.memory_types {
+        type_vec.push(memory_type);
+        heaps.push(Vec::new());
+        heap_growth.push(10);
+    }
 
     DustObjectStore {
-        images: [None; 1000], 
-        free_image_slots: vec![0..999], 
-        free_buffer_slots: vec![0..999], 
-        heap: , 
-        unused_offset: 0
+        ctxt,
+        heap_growth,
+        heaps,
+        types: type_vec,
     }
 }
 
+impl DustObjectStore {
+    pub fn allocate_device_image(&self, image: Image) -> Result<ImageId, DustError> {
+        let image_memory_requirements = unsafe {
+            self.ctxt
+                .logical_device
+                .get_image_memory_requirements(image)
+        };
+
+        let memory_type_index = self.match_memory_type(
+            image_memory_requirements.memory_type_bits,
+            MemoryPropertyFlags::DEVICE_LOCAL,
+        );
+    }
+
+    fn allocate_new_heap(&self, memory_type_index: usize) -> Result<DeviceMemory, DustError> {
+        let (memory_type, growth_amount) = match (
+            self.types.get(memory_type_index),
+            self.heap_growth.get(memory_type_index),
+        ) {
+            (Some(mem_type), Some(growth_amount)) => (mem_type, growth_amount),
+            _ => {
+                return Err(DustError::NoMatchingMemoryType);
+            }
+        };
+
+        let allocate_info = MemoryAllocateInfo::default()
+            .memory_type_index(memory_type_index as u32)
+            .allocation_size(*growth_amount as u64);
+
+        match unsafe {
+            self.ctxt
+                .logical_device
+                .allocate_memory(&allocate_info, None)
+        } {
+            Ok(dm) => Ok(dm),
+            Err(vk_result) => Err(DustError::DeviceMemoryAllocationFailed(vk_result)),
+        }
+    }
+
+    pub fn match_memory_type(
+        &self,
+        filter: u32,
+        matcher: MemoryPropertyFlags,
+    ) -> Result<usize, DustError> {
+        debug!("Testing for memory properties {:?}", matcher);
+        for index in 0..self.types.len() {
+            if (filter & 1 << index) == (1 << index)
+                && (self.types[index as usize].property_flags & matcher) == matcher
+            {
+                return Ok(index);
+            }
+        }
+        Err(DustError::NoMatchingMemoryType)
+    }
+}
